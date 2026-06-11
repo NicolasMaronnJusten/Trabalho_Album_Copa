3ce0802cda990a8ac442d75f0ab2333703e0f5a2
NicolasMaronnJusten <nicolasjustenprofissional@gmail.com>
2026-06-11 01:29:41 -0300

    add Album e Proprietario

---
+++ src/album.java
@@ -0,0 +1,32 @@
+package src;
+
+public class album {
+    //Atributos
+    private String proprietario;
+    private String lista_figurinhas;
+    
+    //Inicializador
+    public album(String proprietario, String lista_figurinhas) {
+        this.proprietario = proprietario;
+        this.lista_figurinhas = lista_figurinhas;
+    }
+    //Metodos
+    private void registrarFigurinhaColada() {
+
+    }
+    private void registrarFigurinhaRepetida() {
+
+    }
+    private void listarFaltantes() {
+
+    }
+    private void listarRepetidas() {
+
+    }
+    private void calcularTotalColadas() {
+
+    }
+    private void calcularTotalRepetidas() {
+
+    }
+}
---
+++ src/proprietario.java
@@ -0,0 +1,14 @@
+package src;
+
+public class proprietario {
+    //Atributos
+    private String nome;
+    private String email;
+    
+    //Inicializador
+    public proprietario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+}
